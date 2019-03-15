d0062a9c31411c9dbc49a9fa625098a126a59452
Preston Sparks <mprestonsparks@gmail.com>
2019-03-15 17:02:26 -0500

    added USD pairs to getLatestRates()

--- main.js
+++ main.js
@@ -18,7 +18,21 @@ function getLatestRates() {
         url: getLatestRatesURL,
         method: "GET"
     }).then(function (response) {
+        var USD = response.rates.USD;
+        var EUR = response.rates.EUR; // Euro
+        var AUD = response.rates.AUD; // Australian Dollar
+        var INR = response.rates.INR; // Indian Rupee
+        // setup USD as base currency (free version uses EUR base)
+        var base = USD/EUR;
+        var USDtoEUR = EUR/base;
+        var USDtoAUD = AUD/base;
+        var USDtoINR = INR/base;
         console.log(response);
+        console.log(base);
+        console.log("USD:EUR ",USDtoEUR);
+        console.log("USD:AUD ",USDtoAUD);
+        console.log("USD:INR ",USDtoINR);
+
     })};
 
 getLatestRates();
